11622a00f4d9653b11bcfee32d4ae7607c82c13a
Mo-ElMumin <120017594+Mo-ElMumin@users.noreply.github.com>
2022-12-07 11:18:15 +0000

    Create 876. Middle of the Linked List.cs

---
+++ 876. Middle of the Linked List.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        
+        ListNode oPos = head;
+        ListNode fPos = head.next; 
+        
+        while (fPos != null)
+        {
+            oPos = oPos.next;
+            fPos = fPos.next;
+            if (fPos != null)
+            {
+                fPos = fPos.next;
+            }
+        }
+        
+        return oPos;
+        
+    }
+}
